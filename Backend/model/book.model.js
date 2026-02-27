4f10e4d480a4e513f0c3794b3091c2495813c280
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-02-27 17:17:41 +0530

    Initial commit

---
+++ Backend/model/book.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    category: String,
+    image: String,
+    title: String,
+});
+const Book = mongoose.model("Book", bookSchema);
+
+export default Book;
+// const book1 = new Book({
+//     name: "Book 1",
+//     price: 10,
+//     category: "Fiction",
+//     image: "https://images-na.ssl-images-amazon.com/images/I/51N-u8AsiDL._SX329_BO1,204,203,200_.jpg",
+//     title: "The Great Gatsby"
+// });
+// book1.save();
